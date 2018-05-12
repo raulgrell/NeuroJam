e4d50194a42e904d6494a38458c0bd1099dcfd20
Ricardo Cacheira <ricardo.cacheira@hotmail.com>
2018-05-12 15:57:53 +0100

    Boss and bullet

--- sketch.js
+++ sketch.js
@@ -1,7 +1,40 @@
+var boss;
+var bullet;
+var canvasHeight = 400;
+var CanvasWidth = 800;
+var shot = false;
+var dist;
+
 function setup() {
-  // put setup code here
+  setFrameRate(60);
+  createCanvas(CanvasWidth, canvasHeight);
+  fill(255);
+  noStroke();
+  boss = new Boss(
+    CanvasWidth - 100,
+    canvasHeight - 200,
+    0.1 * height,
+    10.0,
+    60.0
+  );
+
+  dist = boss.x - 50;
+  console.log(dist);
 }
 
 function draw() {
-  // put drawing code here
+  var deltaTime = 1/getFrameRate();
+  background(0);
+  push();
+  stroke(255);
+  line(50,0,50,1000);
+  pop();
+  boss.display();
+  if (shot)
+  {
+    // console.log("bullet x: " + bullet.x + " bullet speed: "+ bullet.speed);
+    bullet.update(deltaTime);
+    if((millis() - bullet.iMillis) < (bullet.ttc * bullet.tti)*1000)
+    bullet.display();
+  }
 }
